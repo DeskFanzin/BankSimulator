3d59256413e5d14fa553d3690f8fdf76bb41e7da
Gabriel Martins <78751933+DeskFanzin@users.noreply.github.com>
2022-07-21 14:19:38 -0300

    Update Banco.py

--- Banco.py
+++ Banco.py
@@ -1,5 +1,6 @@
 import random
 
+## aqui as contas são criadas, com um número e um saldo.
 class Conta:
     def __init__(self, numero_conta, saldo):
         self.numero_conta = numero_conta
@@ -8,13 +9,14 @@ class Conta:
         return str(self.numero_conta)
 
 class Banco:
+    ##são criados um dicionário para contas, e uma lista para as chaves.
     def __init__(self):
         self.contas = {}
         self.chaves = []
     
     def __repr__(self):
             return str(self.contas)
-
+    ##contas são criadas, com um valor aleatório e com um saldo inicial aleatório.
     def cria_conta(self, numero_contas):
         for i in range(numero_contas):
             numero = random.randint(1, 100)
@@ -22,7 +24,7 @@ class Banco:
             self.contas[numero] = saldo
         for key in self.contas:
             self.chaves.append(key)
-
+            
     def depositar(self, numero, valor):
         for i in range(len(self.contas)):
             if self.chaves[i] == numero:
